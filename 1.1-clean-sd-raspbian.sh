2ffa7d9ca6b37db1590506f7410a0f130e476344
Jacob Gadikian <faddat@gmail.com>
2016-06-11 03:57:38 +0700

    added some cool stuff to the first script.  Did this thing work when you wrote it?

--- 1.1-clean-sd-raspbian.sh
+++ 1.1-clean-sd-raspbian.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+echo "Runnning fdisk -l to list your disks"
+fdisk-l
+echo "Which disk do you want this to run against?  Remember, it'll get wiped!"
+read hdd
 hdd="/dev/sdb"
 umount "$(echo $hdd)1"
 umount "$(echo $hdd)2"
@@ -23,17 +27,19 @@ p
 w" | fdisk $hdd
 mkfs.vfat "$(echo $hdd)1"
 mkdir -p /root/rpi/boot
+kpart
 mount "$(echo $hdd)1" /root/rpi/boot
 
 mkfs.ext4 "$(echo $hdd)2"
 mkdir -p /root/rpi/root
+kpart
 mount "$(echo $hdd)2" /root/rpi/root
 
 if [! -f 2016-03-18-raspbian-jessie-lite.zip]; then
-  tar xzf 2016-03-18-raspbian-jessie-lite.zip -C /root/rpi/root
+  unzip 2016-03-18-raspbian-jessie-lite.zip -d /root/rpi/root
 else
   wget https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2016-03-18/2016-03-18-raspbian-jessie-lite.zip
-  tar xzf 2016-03-18-raspbian-jessie-lite.zip -C /root/rpi/root
+  unzip 2016-03-18-raspbian-jessie-lite.zip -d /root/rpi/root
 fi
 
 sync
